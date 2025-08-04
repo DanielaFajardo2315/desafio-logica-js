f14e29e67ed3bd1927e27770ca96824325fb005c
Daniela Fajardo <danielafajardo2315@gmail.com>
2025-08-04 16:22:19 -0500

    Se agregan los documentos del reto 3

---
+++ reto3/index.js
@@ -0,0 +1,51 @@
+let texto = document.getElementById('texto');
+let numberTabla = document.getElementById('tabla');
+let verT = document.getElementById('btnVer');
+let nombreTabla = document.getElementById('nomTabla')
+let salida = document.getElementById('tablaMultiplicar');
+let botones = document.getElementById('botones');
+let message = document.getElementById('message');
+let salirB = document.getElementById('btnSalir');
+
+function ver(event) {
+    event.preventDefault();
+    let tabla = parseInt(numberTabla.value);
+    nombreTabla.textContent = `Tabla del ${tabla}`
+    salida.style.display = 'block';
+
+    salida.textContent = '';
+
+    for (let i = 1; i <= 10; i ++) {
+        resultado = tabla * i;
+        salida.innerHTML += `${tabla} X ${i} = ${resultado}<br>`;
+    }
+    texto.style.display = 'none';
+    numberTabla.style.display = 'none';
+    verT.style.display = 'none';
+    botones.style.display = 'flex';
+    numberTabla.value = '';
+}
+
+function volver (event) {
+    event.preventDefault();
+
+    nombreTabla.textContent = '';
+    salida.style.display = 'none';
+    texto.style.display = 'block';
+    numberTabla.style.display = 'block';
+    verT.style.display = 'block';
+    botones.style.display = 'none';
+    salida.textContent = '';
+    message.textContent = '';
+    salirB.style.display = 'block';
+}
+
+function salir(event) {
+    event.preventDefault();
+
+    nombreTabla.textContent = '';
+    salida.style.display = 'none';
+    salida.textContent = '';
+    salirB.style.display = 'none';
+    message.textContent = 'Gracias por usar nuestra página, vuelva pronto.';
+}
